96dc7a1470d17fcaff9b57eba4f2a042d7dc62b3
selim121 <selimkhan.bdkhan@gmail.com>
2023-06-15 14:37:22 +0600

    task spark server added

---
+++ index.js
@@ -0,0 +1,108 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 4000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d18ofon.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+
+        const tasksCollection = client.db("taskSpark").collection("taskManagement");
+
+        app.post('/taskCollection', async (req, res) => {
+            try {
+                const task = req.body;
+                const result = await tasksCollection.insertOne(task);
+                res.send(result);
+            } catch (error) {
+                console.error(error);
+                res.status(500).json({ error: 'An error occurred while posting user' });
+            }
+        })
+
+        app.get('/tasks/:email', async (req, res) => {
+            try {
+                const email = req.params.email;
+                const query = { email: email };
+                const result = await tasksCollection.find(query).toArray();
+                result.reverse();
+                res.send(result);
+            } catch (error) {
+                console.error(error);
+                res.status(500).json({ error: 'An error occurred while fetching tasks' });
+            }
+        });
+
+        app.patch('/task/update/:id', async(req, res) => {
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)};
+            const updateDoc = {
+                $set: {
+                    status: 'Completed'
+                }
+            }
+            const result = await tasksCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+        app.put('/update/:id', async(req, res) => {
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)};
+            const updateTask = req.body;
+            const updateDoc = {
+                $set: {
+                    title: updateTask.title,
+                    description: updateTask.description,
+                    status: updateTask.status,
+                }
+            };
+            const result = await tasksCollection.updateOne(filter, updateDoc);
+            res.send(result);
+        })
+
+        app.delete('/task/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await tasksCollection.deleteOne(query);
+            res.send(result);
+        })
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Task Spark is running...')
+})
+
+app.listen(port, () => {
+    console.log(`Task Spark is running on port ${port}`);
+})
